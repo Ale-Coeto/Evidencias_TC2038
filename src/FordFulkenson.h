82b8000ebeb93a7ce0c517fa143749e9cb48d9bf
Alejandra Coeto <a01285221@tec.mx>
2024-11-29 16:20:47 -0600

    Fix sonar

--- src/FordFulkenson.h
+++ src/FordFulkenson.h
@@ -1,7 +1,21 @@
 /*
- * Oscar Arreola (A01178076), Mariana Amy Martínez (A00836245) y Alejandra Coeto (A01285221)
- * Última modificación: Domingo 29 de Noviembre
- * Implementación del algoritmo de Ford Fulkenson para encontrar el flujo máximo en un grafo
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * http://github.com/SonarOpenCommunity/sonar-cxx
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifndef FORD_FULKENSON
