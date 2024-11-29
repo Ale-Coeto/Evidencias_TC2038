39c0d5974f01026368b436d4377eb402c8aeb88b
Oscar Arreola <oscar.arreola.jr@gmail.com>
2024-11-29 16:14:48 -0600

    format

--- src/LinearSearch.h
+++ src/LinearSearch.h
@@ -8,12 +8,20 @@ using namespace std;
 
 namespace algorithms
 {
-    double computeDistance(pair<int, int> p1, pair<int, int> p2)
+    /**
+        Encontrar distancia entre 2 puntos
+        Complejidad: O(1)
+    */
+    double compute_distance(pair<int, int> p1, pair<int, int> p2)
     {
         return sqrt(pow(p1.first - p2.first, 2.0) + pow(p1.second - p2.second, 2.0));
     }
 
-    vector<pair<int, int> > closestCentral(vector<pair<int, int> > centrals, pair<int, int> newCentral)
+    /**
+        Encontrar centrales más cercanas a nuevo servicio
+        Complejidad: O(n) -> n es el numero de centrales
+    */
+    vector<pair<int, int> > closest_central(vector<pair<int, int> > centrals, pair<int, int> newCentral)
     {
         if (centrals.size() == 0)
         {
@@ -22,11 +30,11 @@ namespace algorithms
         }
 
         vector<pair<int, int> > closest = {centrals[0]};
-        double minDiff = computeDistance(newCentral, closest[0]);
+        double minDiff = compute_distance(newCentral, closest[0]);
 
         for (int i = 1; i < centrals.size(); i++)
         {
-            double distance = computeDistance(centrals[i], newCentral);
+            double distance = compute_distance(centrals[i], newCentral);
             if (distance < minDiff)
             {
                 closest = {centrals[i]};
@@ -47,7 +55,7 @@ namespace algorithms
 //     vector<pair<int, int> > centrals{{200, 500}, {300, 100}, {450, 150}, {520, 480}};
 //     pair<int, int> target{400, 300};
 
-//     vector<pair<int, int> > res = algorithms::closestCentral(centrals, target);
+//     vector<pair<int, int> > res = algorithms::closest_central(centrals, target);
 
 //     for (auto x : res)
 //     {
