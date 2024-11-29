becbec1c899b23bfdfb24847608971a071f2fe1e
Alejandra Coeto <a01285221@tec.mx>
2024-11-29 13:38:27 -0600

    Added main tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,15 +1,48 @@
 #include <cassert>
 #include <iostream>
+#include <fstream>
+#include <catch2/catch_test_macros.hpp>
 
-int add(int a, int b) {
-    return a + b;
+using namespace std;
+
+TEST_CASE("Check file can be opened") {
+    const std::string expectedOutputFile = "expected_output.txt";
+
+    // Print debugging information
+    std::cout << "Current directory: " << std::filesystem::current_path() << std::endl;
+    std::cout << "Trying to open: " << expectedOutputFile << std::endl;
+
+    std::ifstream expectedFile(expectedOutputFile);
+    REQUIRE(expectedFile.is_open()); // Assert file opened successfully
 }
 
-int main() {
-    // Basic test cases
-    assert(add(2, 3) == 5);
-    assert(add(-1, 1) == 0);
+TEST_CASE("Main Functionality") {
+    // Paths to executable and files
+    const string executable = "./Evidencia2";
+    const string tempOutputFile = "temp_output.txt";
+    const string expectedOutputFile = "expected_output.txt";
+    const string inputFile = "../src/in.txt";
 
-    std::cout << "All tests passed!" << std::endl;
-    return 0;
+    // Run the executable and redirect output to a temporary file
+    const string command = executable + " < " + inputFile + " > " + tempOutputFile;
+    REQUIRE(system(command.c_str()) == 0);
+
+    // // Read the temporary output file
+    ifstream tempFile(tempOutputFile);
+    REQUIRE(tempFile.is_open());
+    string tempOutput((istreambuf_iterator<char>(tempFile)),
+                            istreambuf_iterator<char>());
+
+    // // Read the expected output file
+    ifstream expectedFile(expectedOutputFile);
+    REQUIRE(expectedFile.is_open());
+    string expectedOutput((istreambuf_iterator<char>(expectedFile)),
+                                istreambuf_iterator<char>());
+    
+    cout << "Temp Output: " << tempOutput << endl;
+    // Compare the two outputs
+    REQUIRE(tempOutput == expectedOutput);
+
+    // Clean up
+    std::remove(tempOutputFile.c_str());
 }
