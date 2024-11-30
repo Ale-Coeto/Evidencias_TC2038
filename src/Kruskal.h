d14ca3e49d39e097bb460e802aaddd29e495eccd
Oscar Arreola <oscar.arreola.jr@gmail.com>
2024-11-30 12:10:10 -0600

    changes

--- src/Kruskal.h
+++ src/Kruskal.h
@@ -13,12 +13,18 @@
 using namespace std;
 #define INF 10000000
 
-struct edge
+/**
+  class Edge
+
+    Used to represent edges in the adjacency graph.
+
+ */
+struct Edge
 {
-    int first;
-    int second;
-    int weight;
-    edge(int f, int s, int w)
+    int first;  // The first element
+    int second; // The second element
+    int weight; // The edge weight
+    Edge(int f, int s, int w)
     {
         this->first = f;
         this->second = s;
@@ -26,62 +32,70 @@ struct edge
     }
 };
 
-bool operator<(edge &a, edge &b)
+bool operator<(Edge &a, Edge &b)
 {
     return a.weight < b.weight;
 }
 
-namespace algorithms {
+namespace algorithms
+{
 
-    class DSU { 
-        int* parent; 
-        int* rank; 
-  
-    public: 
-        DSU(int n) 
-        { 
-            parent = new int[n]; 
-            rank = new int[n]; 
-    
-            for (int i = 0; i < n; i++) { 
-                parent[i] = -1; 
-                rank[i] = 1; 
-            } 
-        } 
-    
-        // Find function 
-        int find(int i) 
-        { 
-            if (parent[i] == -1) 
-                return i; 
-    
-            return parent[i] = find(parent[i]); 
-        } 
-    
-        // Union function 
-        void unite(int x, int y) 
-        { 
-            int s1 = find(x); 
-            int s2 = find(y); 
-    
-            if (s1 != s2) { 
-                if (rank[s1] < rank[s2]) { 
-                    parent[s1] = s2; 
-                } 
-                else if (rank[s1] > rank[s2]) { 
-                    parent[s2] = s1; 
-                } 
-                else { 
-                    parent[s2] = s1; 
-                    rank[s1] += 1; 
-                } 
-            } 
-        } 
-    }; 
+    class DSU
+    {
+        int *parent;
+        int *rank;
 
-    vector<pair<int,int>> kruskal(int size, vector<edge> &edges){
+    public:
+        DSU(int n)
+        {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                parent[i] = -1;
+                rank[i] = 1;
+            }
+        }
+
+        // Find function
+        int find(int i)
+        {
+            if (parent[i] == -1)
+                return i;
+
+            return parent[i] = find(parent[i]);
+        }
+
+        // Union function
+        void unite(int x, int y)
+        {
+            int s1 = find(x);
+            int s2 = find(y);
+
+            if (s1 != s2)
+            {
+                if (rank[s1] < rank[s2])
+                {
+                    parent[s1] = s2;
+                }
+                else if (rank[s1] > rank[s2])
+                {
+                    parent[s2] = s1;
+                }
+                else
+                {
+                    parent[s2] = s1;
+                    rank[s1] += 1;
+                }
+            }
+        }
+    };
+
+    vector<pair<int, int> > kruskal(int size, vector<Edge> &edges)
+    {
         sort(edges.begin(), edges.end());
-        vector<pair<int,int>> answer;
+        vector<pair<int, int> > answer;
         DSU s(size);
         for (auto edge : edges)
         {
